175acd362516a0c024fc36445dbed7fa41aeba85
JustinDeanS <143033461+JustinDeanS@users.noreply.github.com>
2024-06-02 20:02:53 -0400

    added # for package

--- automation.py
+++ automation.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 # from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 # Specify the path to the ChromeDriver
